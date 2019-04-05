aa58621abd4f4877f754798e67d206cab532ae27
yamini-kabra <45656567+yamini-kabra@users.noreply.github.com>
2019-04-05 19:10:35 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+var http = require('http');
+var fs = require('fs');
+
+const PORT=3000; 
+
+fs.readFile('./new.html', function (err, html) {
+
+    if (err)throw err; 
+    
+
+    http.createServer(function(req, res) {  
+        res.writeHeader(200, {"Content-Type": "text/html"});  
+        res.write(html);  
+        res.end(); 
+        
+
+    }).listen(PORT);
+});
